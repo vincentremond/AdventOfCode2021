1dfb122c32e425ff83843863ed3b61b300894d7d
Vincent Rémond <vincent.remond@gmail.com>
2021-12-03 16:19:16 +0100

    Day 03 (part 2)

--- AdventOfCode2021.Common/Library.fs
+++ AdventOfCode2021.Common/Library.fs
@@ -6,3 +6,21 @@ open System.Text.RegularExpressions
 module String =
 
     let splitLines s = Regex.Split(s, "[\r\n]+")
+
+[<RequireQualifiedAccess>]
+module List =
+    let tryPop l =
+        match l with
+        | head :: tail -> Some(head, tail)
+        | [] -> None
+
+[<RequireQualifiedAccess>]
+module Option =
+    let ofList (list: List<Option<'a>>) : Option<List<'a>> =
+        List.foldBack
+            (fun item acc ->
+                match item, acc with
+                | Some value, Some list -> Some(value :: list)
+                | _ -> None)
+            list
+            (Some [])
