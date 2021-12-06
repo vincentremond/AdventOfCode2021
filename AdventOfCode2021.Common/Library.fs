be3ef03377ecde6ac9aa067ee6eca5343eaeca63
Vincent Rémond <vremond@d-edge.com>
2021-12-06 08:44:11 +0100

    Day 06

--- AdventOfCode2021.Common/Library.fs
+++ AdventOfCode2021.Common/Library.fs
@@ -26,11 +26,17 @@ module Option =
             list
             (Some [])
 
-
 [<RequireQualifiedAccess>]
 module Tuple =
     let map f (a, b) = (f a, f b)
     let map2 f (a1, a2) (b1, b2) = ((f a1 b1), (f a2 b2))
+    let mapSnd f (a, b) = (a, f b)
     let apply2 (f1, f2) (a1, a2) (b1, b2) = ((f1 a1 b1), (f2 a2 b2))
     let transpose ((a1, a2), (b1, b2)) = ((a1, b1), (a2, b2))
     let map3 f (a1, a2) (b1, b2) (c1, c2) = ((f a1 b1 c1), (f a2 b2 c2))
+    let rev (a, b) = (b, a)
+
+[<RequireQualifiedAccess>]
+module Seq =
+    let countBy64 f l =
+        l |> Seq.countBy f |> Seq.map (Tuple.mapSnd int64)
