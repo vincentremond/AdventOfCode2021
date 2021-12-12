3fa0544e8a21a159230859f55b0e8adee914008e
Vincent Rémond <vincent.remond@gmail.com>
2021-12-12 17:43:29 +0100

    Day 12

---
+++ AdventOfCode2021.Day12.Tests/UnitTest1.fs
@@ -0,0 +1,105 @@
+module AdventOfCode2021.Day12.Tests
+
+open System.IO
+open AdventOfCode2021.Common
+open AdventOfCode2021.Day12
+open NUnit.Framework
+open Swensen.Unquote
+
+let getInputs () = "inputs.txt" |> File.ReadAllLines
+
+let smallSample () =
+    (10,
+     36,
+     "
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+")
+
+let largerSample () =
+    (19,
+     103,
+     "
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+")
+
+let eventLargerSample () =
+    (226,
+     3509,
+     "
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+")
+
+[<Test>]
+let ``1-1 Test part1 with samples`` () =
+
+    [|
+        smallSample ()
+        largerSample ()
+        eventLargerSample ()
+    |]
+    |> Seq.iter
+        (fun (expectedPathsCountPart1, _, inputString) ->
+            let asLines =
+                inputString
+                |> String.splitLines
+                |> (Array.filter String.notNullOrEmpty)
+
+            test <@ Solution.part1 asLines = expectedPathsCountPart1 @>)
+
+[<Test>]
+let ``1-2 Test part1 with inputs`` () =
+    test <@ Solution.part1 (getInputs ()) = 3463 @>
+
+let p2 f = 
+    let _, expected, data = f()
+    let input =
+        data
+        |> String.splitLines
+        |> (Array.filter String.notNullOrEmpty)
+
+    Assert.AreEqual(expected, (Solution.part2 input))
+
+[<Test>]
+let ``2-1-1 Test part1 with samples`` () = p2 smallSample
+
+[<Test>]
+let ``2-1-2 Test part1 with samples`` () = p2 largerSample
+
+[<Test>]
+let ``2-1-3 Test part1 with samples`` () = p2 eventLargerSample
+
+[<Test>]
+let ``2-2 Test part1 with inputs`` () =
+    Assert.AreEqual(91533, (Solution.part2 (getInputs ())))
---
+++ AdventOfCode2021.Day12/Library.fs
@@ -0,0 +1,100 @@
+﻿namespace AdventOfCode2021.Day12
+
+open System
+open FSharp.Text.RegexProvider
+open AdventOfCode2021.Common
+
+type LineParser = Regex<"^(?<From>\w+)-(?<To>\w+)$">
+
+type CaveId = string
+
+type Cave =
+    | Start
+    | End
+    | LargeCave of CaveId
+    | SmallCave of CaveId
+
+module Solution =
+
+    let (|AllUpperCase|AllLowerCase|) (s: string) =
+        if s |> Seq.forall Char.IsUpper then
+            AllUpperCase
+        else
+            AllLowerCase
+
+    let calc lines canUseJoker =
+
+        let toCave s =
+            match s with
+            | "start" -> Start
+            | "end" -> End
+            | AllUpperCase -> LargeCave s
+            | AllLowerCase -> SmallCave s
+
+        let readLine l =
+            let r = l |> (LineParser().TypedMatch)
+
+            [|
+                (toCave r.From.Value, toCave r.To.Value)
+                (toCave r.To.Value, toCave r.From.Value)
+            |]
+
+        let caves =
+            lines
+            |> Array.collect readLine
+            |> Array.groupByFst
+            |> Map.ofArray
+
+        let displayPath path =
+            path
+            |> Seq.rev
+            |> Seq.iter
+                (fun p ->
+                    let itemValue =
+                        match p with
+                        | Start -> "start"
+                        | End -> "end"
+                        | SmallCave s -> s
+                        | LargeCave l -> l
+
+                    printf "%s," itemValue)
+
+            printfn "End"
+
+        let rec explore cave exploredSmallCaves jokerUsed path =
+            let newPath = cave :: path
+
+            let exploredSmallCaves =
+                match cave with
+                | SmallCave smallCaveId ->
+                    match (Set.contains smallCaveId exploredSmallCaves) with
+                    | false -> Set.add smallCaveId exploredSmallCaves
+                    | _ -> exploredSmallCaves
+                | _ -> exploredSmallCaves
+
+            let possibleDirections = caves |> Map.find cave
+
+            possibleDirections
+            |> Array.sumBy
+                (fun targetCave ->
+                    match targetCave with
+                    | Start -> 0
+                    | End ->
+                        displayPath newPath
+                        1
+                    | LargeCave _ -> explore targetCave exploredSmallCaves jokerUsed newPath
+                    | SmallCave smallCaveId ->
+                        let explored =
+                            Set.contains smallCaveId exploredSmallCaves
+
+                        match explored, jokerUsed, canUseJoker with
+                        | true, false, true -> explore targetCave exploredSmallCaves true newPath
+                        | true, true, true -> 0
+                        | true, _, false -> 0
+                        | false, _, _ -> explore targetCave exploredSmallCaves jokerUsed newPath
+                )
+
+        explore Start Set.empty false []
+
+    let part1 lines = calc lines false
+    let part2 lines = calc lines true
