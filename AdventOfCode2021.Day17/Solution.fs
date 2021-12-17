7d8e790178cf033e6edb53c99215ef03b0e2d57e
Vincent Rémond <vincent.remond@gmail.com>
2021-12-17 23:17:31 +0100

    Day 17 (final)

--- AdventOfCode2021.Day17/Solution.fs
+++ AdventOfCode2021.Day17/Solution.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.Text.RegularExpressions
-open AdventOfCode2021.Day17
 
 [<Measure>]
 type Step
@@ -58,49 +57,7 @@ module Solution =
             MaxY = getV "MaxY"
         }
 
-    let incHPos (pos: int<HPos>) (vX: int<HSpeed>) = (pos + (vX * 1<Step>))
     let incVPos (pos: int<VPos>) (vX: int<VSpeed>) = (pos + (vX * 1<Step>))
-
-    let tryResolveVX (minX: int<HPos>) (maxX: int<HPos>) (vX: int<HSpeed>) =
-        let rec step (pos: int<HPos>) (idx: int<Step>) (vX': int<HSpeed>) =
-            if pos >= minX && pos <= maxX then
-                Some(idx, vX)
-            else if pos > maxX then
-                None
-            else if vX' = 0<HSpeed> then
-                None
-            else
-                step (incHPos pos vX') (idx + 1<Step>) (vX' - 1<HSpeed>)
-
-        step (incHPos 0<HPos> vX) 1<Step> vX
-
-    let getPossibleVX (minX: int<HPos>) (maxX: int<HPos>) : (int<Step> * int<HSpeed>) list =
-
-        let maxSpeed = maxX / 1<Step>
-
-        [ maxSpeed .. -1<HSpeed> .. 1<HSpeed> ]
-        |> List.choose (tryResolveVX minX maxX)
-
-    let calcInitialVerticalSpeed (p: int<VPos>) (s: int<Step>) =
-        let p = p |> decimal
-        let s = s |> decimal
-        (p + ((s * (s - 1M)) / 2M)) / s
-
-    let searchMaxVerticalSpeed (minY: int<VPos>) (maxY: int<VPos>) (steps: int<Step>) : (int<VSpeed> * int<VSpeed>) option =
-        let maxVSpeed =
-            calcInitialVerticalSpeed maxY steps
-            |> Math.Floor
-            |> int
-
-        let minVSpeed =
-            calcInitialVerticalSpeed minY steps
-            |> Math.Ceiling
-            |> int
-
-        if minVSpeed <= maxVSpeed then
-            Some(maxVSpeed * 1<VSpeed>, minVSpeed * 1<VSpeed>)
-        else
-            None
 
     let calcHPosAtStep (initialHSpeed: int<HSpeed>) (step: int<Step>) : int<HPos> =
         let step = step |> int
@@ -111,27 +68,26 @@ module Solution =
         step * initialHSpeed - ((step * (step - 1)) / 2)
         |> LanguagePrimitives.Int32WithMeasure
 
-    let calcVPosAtStep (initialVSpeed: int<VSpeed>) (step: int<Step>) : int<VPos> =
-        let step = step |> int
-        let initialVSpeed = initialVSpeed |> int
+    let getMax _ possibleVSpeeds =
+        possibleVSpeeds
+        |> Seq.map (fun (_, vPos: int<VPos>, _) -> vPos)
+        |> Seq.max
 
-        step * initialVSpeed - ((step * (step - 1)) / 2)
-        |> LanguagePrimitives.Int32WithMeasure
+    let getDifferentCount possibleHSpeeds possibleVSpeeds =
+        Seq.allPairs possibleHSpeeds possibleVSpeeds
+        |> Seq.choose
+            (fun ((hStep, hSpeed, verticalDrop), (vStep, _, vSpeed)) ->
+                if hStep = vStep then
+                    Some(hSpeed, vSpeed)
+                else if hStep < vStep && verticalDrop then
+                    Some(hSpeed, vSpeed)
+                else
+                    None)
+        |> Seq.distinct
+        |> Seq.toArray
+        |> Array.length
 
-    let findPeak initialVSpeed maxSteps =
-        [ 1<Step> .. 1<Step> .. maxSteps ]
-        |> List.map (calcVPosAtStep initialVSpeed)
-        |> List.max
-
-    let getPossibleVSpeeds target =
-        let rec possibleVSpeeds' currentStep acc =
-            match searchMaxVerticalSpeed target.MinY target.MaxY currentStep with
-            | None -> acc
-            | Some (min, max) -> possibleVSpeeds' (currentStep + 1<Step>) ((currentStep, (min, max)) :: acc)
-
-        possibleVSpeeds' 1<Step> []
-
-    let calc input =
+    let calc getResult input =
         let target = parseInput input
 
         let possibleHSpeeds =
@@ -169,75 +125,39 @@ module Solution =
                     stepsInRange)
             |> List.concat
 
-        let possibleVSpeed =
-            [|
+        let getPossibleVSpeeds (speed: int<VSpeed>) : (int<Step> * int<VPos> * int<VSpeed>) list =
+            let rec get' step pos spd currMax acc =
+                let pos = incVPos pos spd
+
+                let currMax =
+                    match currMax with
+                    | None -> Some pos
+                    | Some m -> Some(max pos m)
+
+
+                let next =
+                    get' (step + 1<Step>) pos (spd - 1<VSpeed>) currMax
+
+                if pos >= target.MinY && pos <= target.MaxY then
+                    next ((step, currMax |> Option.get, speed) :: acc)
+                else if pos >= target.MinY then
+                    next acc
+                else
+                    acc
+
+            get' 1<Step> 0<VPos> speed None []
+
+        let possibleVSpeeds =
+            [
                 target.MinY .. 1<VPos> .. ((target.MinY |> abs) - 1<VPos>)
-            |] |> List.map (fun possibleVSpeed -> )
+            ]
+            |> List.map
+                (fun possibleVSpeed ->
+                    let possibleVSpeed = (possibleVSpeed |> int) * 1<VSpeed>
+                    getPossibleVSpeeds possibleVSpeed)
+            |> List.concat
 
+        getResult possibleHSpeeds possibleVSpeeds
 
-
-
-
-        possibleHSpeeds
-    //
-//        let validValues =
-//            [ 2 .. (target.MaxX |> int) ]
-//            |> List.map
-//                (fun force ->
-//                    [ 1 .. (force - 1) ]
-//                    |> List.map
-//                        (fun hSpeed ->
-//                            let vSpeed = (force - hSpeed) * 1<VSpeed>
-//                            let hSpeed = hSpeed * 1<HSpeed>
-//
-//                            //let unfold (state:'state) : ('t * 'state) option =
-//                            let unfold (currentStep: int<Step>) : ((int<Step> * int<VPos>) option * int<Step>) option =
-//                                let x = calcVPosAtStep vSpeed currentStep
-//
-//                                if x <= target.MaxY && x >= target.MinY then
-//                                    Some(Some(currentStep, x), currentStep + 1<Step>)
-//                                else if x < target.MinY then
-//                                    None
-//                                else
-//                                    Some(None, currentStep + 1<Step>)
-//
-//                            let stepsInRange =
-//                                List.unfold unfold 1<Step> |> List.choose id
-//
-//
-//                            let choose =
-//                                stepsInRange
-//                                |> List.map
-//                                    (fun (step, vPos) ->
-//                                        let hPos = calcHPosAtStep hSpeed step
-//
-//                                        if hPos >= target.MinX && hPos <= target.MaxX then
-//                                            let peak = findPeak vSpeed step
-//
-//
-//                                            Some
-//                                                {
-//                                                    Steps = step
-//                                                    Peak = peak
-//                                                    InitialHSpeed = hSpeed
-//                                                    InitialVSpeed = vSpeed
-//                                                    PosX = hPos
-//                                                    PosY = vPos
-//
-//                                                }
-//                                        else
-//                                            None)
-//                                |> List.choose id
-//
-//                            choose)
-//
-//                    )
-//            |> List.concat
-//            |> List.concat
-//            |> List.sortByDescending (fun r -> r.Peak)
-//
-//        let x = validValues |> List.head
-//        x.Peak
-
-    let part1 = calc
-    let part2 = calc
+    let part1 = calc getMax
+    let part2 = calc getDifferentCount
