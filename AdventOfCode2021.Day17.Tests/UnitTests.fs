0d7f97ac8d21e939c570320b6fbb43989e2b9076
Vincent Rémond <vincent.remond@gmail.com>
2021-12-17 23:17:31 +0100

    Day 17 (final)

--- AdventOfCode2021.Day17.Tests/UnitTests.fs
+++ AdventOfCode2021.Day17.Tests/UnitTests.fs
@@ -10,12 +10,12 @@ let getInputs () = "target area: x=217..240, y=-126..-69"
 [<Test>]
 let ``1-1 Test part1 with sample`` () = (45 , (Solution.part1 (getSample ()))) |> Assert.AreEqual
 [<Test>]
-let ``1-2 Test part1 with inputs`` () = (-1 , (Solution.part1 (getInputs ()))) |> Assert.AreEqual
+let ``1-2 Test part1 with inputs`` () = (7875 , (Solution.part1 (getInputs ()))) |> Assert.AreEqual
 
 [<Test>]
-let ``2-1 Test part1 with sample`` () = (-1 , (Solution.part2 (getSample ()))) |> Assert.AreEqual
+let ``2-1 Test part1 with sample`` () = (112 , (Solution.part2 (getSample ()))) |> Assert.AreEqual
 [<Test>]
-let ``2-2 Test part1 with inputs`` () = (-1 , (Solution.part2 (getInputs ()))) |> Assert.AreEqual
+let ``2-2 Test part1 with inputs`` () = (2321 , (Solution.part2 (getInputs ()))) |> Assert.AreEqual
 
 [<EntryPoint>]
 let main _ = 0
